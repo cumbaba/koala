0e9e67386f4f54ade4c58f2233b4b2e55f330e23
Cumhur Sayan <cumhur.sayan@macio.de>
2020-07-04 11:43:57 +0200

    sample map styles and marker icon

--- pages/SearchScreen.js
+++ pages/SearchScreen.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
-import { SafeAreaView, ScrollView, Text, View,StatusBar, TouchableOpacity, StyleSheet } from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
-import places from '.././data/places.json';
+import places from '.././dummy-json-data/places.json';
+
+import mapStyles from '.././styles/map/styles.json';
 
 export default class SearchScreen extends React.Component {
   render() {
     return (
-
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <View style={styles.MainContainer}>
-              <MapView
-                style={styles.mapStyle}
-                showsUserLocation={true}
-                zoomEnabled={true}
-                zoomControlEnabled={true}
-                initialRegion={{
-                  latitude: places.data[0].coordinates.latitude,
-                  longitude: places.data[0].coordinates.longitude,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}>
-            {
-              places.data.map(marker => (
-                <MapView.Marker 
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.MainContainer}>
+          <MapView
+            style={styles.mapStyle}
+            customMapStyle={mapStyles.night_mode}
+            showsUserLocation={true}
+            zoomEnabled={true}
+            zoomControlEnabled={true}
+            initialRegion={{
+              latitude: places.data[0].coordinates.latitude,
+              longitude: places.data[0].coordinates.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}>
+            {places.data.map(marker => (
+              <MapView.Marker
                 key={marker.id}
-                  coordinate={marker.coordinates}
-                  title={marker.title}
+                anchor={{x: 0, y: 1}} // TODO position the anchor after the icons have been enhanced
+                coordinate={marker.coordinates}
+                title={marker.title}
+                image={require('./../icons/map_icons/sports/basketball.png')}
               />
             ))}
-              </MapView>
-          </View>
-    </View>
+          </MapView>
+        </View>
+      </View>
     );
   }
 }
